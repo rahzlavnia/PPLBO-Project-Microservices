44b1b12ac978390df4179aee0a5d8fd4d05c281f
rahzlavnia <sarah.ajjh69@gmail.com>
2025-02-14 11:19:28 +0700

    update part 4: sync comm using OpenFeign & testing with Wiremock

--- order-service/order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
+++ order-service/order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.techie.microservices.order.service;
 
+import com.techie.microservices.order.client.InventoryClient;
 import com.techie.microservices.order.dto.OrderRequest;
 import com.techie.microservices.order.model.Order;
 import com.techie.microservices.order.repository.OrderRepository;
@@ -13,12 +14,24 @@ import java.util.UUID;
 public class OrderService {
 
     private  final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
+
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+
+        // 1. Using Mockito
+        // 2. Use Wiremock (better, actually mock out)
+
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
+        }
     }
 }
